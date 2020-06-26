db003cf96cc27f48ea458586711eebe943a07982
= <=>
2020-06-26 04:30:40 -0300

    add the list of Api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import api from './services/api'
 import "./styles.css";
 
+
 function App() {
+
+    //var de estado
+  const [projects, setProjects] = useState([])
+
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      
+  })
+ },[])
+
+
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+    "title": "teste2",
+    "url": "www.ta.com.br",
+    "techs": ["Node","React"]}
+    )
+      const project = response.data
+
+      setProjects([...projects, project])
+
+      console.log(response.data)
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    // TODO list:  {projects.map(project =>  <li id={project.id} key={project.id}  > {project.title}  })
+     
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+      <li>            
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
